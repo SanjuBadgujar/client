7a62ceb7760eafbf21e11c20949d5971ac58cb29
Sanju@123 <sanjubadgujar112@gmail.com>
2024-04-30 17:02:21 +0530

    Your commit message here

--- src/Component/Car/Car.js
+++ src/Component/Car/Car.js
@@ -1,35 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
 function Car() {
-  const [student, setStudent] = useState({});
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/api/student")
-      .then((res) =>res.json() )
-      .then((data) => setStudent(data.data[0])
-      );
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data.data);
+      });
   }, []);
+
   return (
     <div className="Car">
-       <div className='table'>
-       <table>
-        <thead>
-          <tr>
-            <th>student name</th>
-            <th>age</th>
-           
-          </tr>
-        </thead>
-        <tbody>.
-          <tr>
-            <td>{student.name}</td>
-            <td>{student.age}</td>
-            
-          </tr>
-        
-        </tbody>
-      </table>
-       </div>
+      <div className='table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Student name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student, index) => (
+              <tr key={index}>
+                <td>{student.name}</td>
+                <td>{student.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
